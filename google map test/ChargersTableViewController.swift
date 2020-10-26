b1133e4b2ce1bade172f09fada31d53bdd2dc002
minu <hayannoon95@gmail.com>
2020-10-26 18:28:20 +0900

    add tableview controller

---
+++ google map test/ChargersTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ChargersTableViewController.swift
+//  google map test
+//
+//  Created by 정민우 on 2020/10/26.
+//  Copyright © 2020 hayannoon. All rights reserved.
+//
+
+import UIKit
+
+class ChargersTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
